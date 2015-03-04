01446c3c60661283bdca4bf92ef7e39b84661a93
Rilindo Foster <rilindo.foster@monzell.com>
2015-03-03 20:28:00 -0600

    updated metadata

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,7 @@ maintainer_email 'rilindo.foster@monzell.com'
 license          'All rights reserved'
 description      'Installs/Configures wync'
 long_description 'Installs/Configures wync'
-version          '0.1.2'
+version          '0.1.3'
+%w{ ubuntu debian redhat centos suse }.each do |os|
+supports os
+end
